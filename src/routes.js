c18fc838b77a7b073d5fda01573a54d8b9d69136
Amruta Shete <ashete@mobiquityinc.com>
2019-02-28 12:44:29 +0530

    Completed till week 4

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,57 @@
 (function () {
   'use strict';
-  
+
   angular.module('ShoppingList')
-  .config(RoutesConfig);
-  
+    .config(RoutesConfig);
+
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
-  
+
     // Redirect to home page if no other URL matches
     $urlRouterProvider.otherwise('/');
-  
+
     // *** Set up UI states ***
     $stateProvider
-  
-    // Home page
-    .state('home', {
-      url: '/',
-      templateUrl: 'src/shoppingListPremade/template/home.template.html'
-    })
-  
-    // Premade list page
-    .state('mainList', {
-      url: '/main-list',
-      templateUrl: 'src/shoppingListPremade/template/main-shoppinglist.template.html',
-      controller: 'MainShoppingListController as mainList'
-    });
+
+      // Home page
+      .state('home', {
+        url: '/',
+        templateUrl: 'src/shoppingListPremade/template/home.template.html'
+      })
+
+      // Premade list page
+      .state('mainList', {
+        url: '/main-list',
+        templateUrl: 'src/shoppingListPremade/template/main-shoppinglist.template.html',
+        controller: 'MainShoppingListController as mainList',
+        resolve: {
+          items: ['ShoppingListService', function (ShoppingListService) {
+            return ShoppingListService.getItems();
+          }]
+        }
+      })
+      .state('mainList.itemDetail', {
+        // url: '/item-detail/{itemId}',
+        templateUrl: 'src/shoppingListPremade/template/item-detail.template.html',
+        controller: 'ItemDetailController as itemdetail',
+        params: {
+          itemId: null
+        }
+      });
+      // .state('itemDetail', {
+      //   url: '/item-detail/{itemId}',
+      //   templateUrl: 'src/shoppingListPremade/template/item-detail.template.html',
+      //   controller: 'ItemDetailController as itemdetail',
+      //   resolve: {
+      //     item: ['$stateParams', 'ShoppingListService',
+      //       function ($stateParams, ShoppingListService) {
+      //         return ShoppingListService.getItems()
+      //         .then(function(items) {
+      //           return items[$stateParams.itemId];
+      //         })
+      //       }]
+      //   }
+      // });
   }
-  
-  })();
+
+})();
--- src/spinner/loadingspinner.component.js
+++ src/spinner/loadingspinner.component.js
@@ -11,21 +11,46 @@
   SpinnerController.$inject = ['$rootScope']
   function SpinnerController($rootScope) {
     var $ctrl = this;
-  
-    var cancelListener = $rootScope.$on('shoppinglist:processing', function (event, data) {
-      console.log("Event: ", event);
-      console.log("Data: ", data);
-  
-      if (data.on) {
+    var cancellers  = [];
+
+    $ctrl.$onInit = function(){
+      var cancel = $rootScope.$on('$stateChangeStart', 
+      function(event, toState, toParams, fromState, fromParams, option){
         $ctrl.showSpinner = true;
-      }
-      else {
+      });
+      cancellers.push(cancel);
+
+
+      var cancel = $rootScope.$on('$stateChangeSuccess', 
+      function(event, toState, toParams, fromState, fromParams){
         $ctrl.showSpinner = false;
-      }
-    });
+      });
+      cancellers.push(cancel);
+
+      var cancel = $rootScope.$on('$stateChangeError', 
+      function(event, toState, toParams, fromState, fromParams, error){
+        $ctrl.showSpinner = false;
+      });
+      cancellers.push(cancel);
+    };
+  
+    // var cancelListener = $rootScope.$on('shoppinglist:processing', function (event, data) {
+    //   console.log("Event: ", event);
+    //   console.log("Data: ", data);
+  
+    //   if (data.on) {
+    //     $ctrl.showSpinner = true;
+    //   }
+    //   else {
+    //     $ctrl.showSpinner = false;
+    //   }
+    // });
   
     $ctrl.$onDestroy = function () {
-      cancelListener();
+      // cancelListener();
+      cancellers.forEach(function(item){
+        item();
+      })
     };
   
   };
